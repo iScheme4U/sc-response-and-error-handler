2383458df728488a573359ffedfc38cfe7b2bce2
Scott Lau <exceedego@126.com>
2022-03-15 10:52:47 +0800

    添加错误码

--- src/main/java/com/soulcraft/network/resp/error/FileResponseEnum.java
+++ src/main/java/com/soulcraft/network/resp/error/FileResponseEnum.java
@@ -21,6 +21,9 @@ public enum FileResponseEnum implements BusinessExceptionAssert {
     FILE_SIZE_EXCEEDS_THRESHOLD(902, "File {0} size exceeds threshold {1}"),
     FILENAME_CONTAINS_ILLEGAL_CHARACTER(903, "Filename {0} contains illegal character."),
     CANNOT_CREATE_DIRECTORY(904, "Cannot create directory {0}."),
+    DIRECTORY_NOT_FOUND(905, "Directory {0} not found."),
+    FILE_NOT_FOUND(906, "File {0} not found."),
+    FILE_CANNOT_BE_SAVED(907, "File {0} cannot be saved."),
     ;
 
     /**
